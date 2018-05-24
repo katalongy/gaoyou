a7af1637973deaf191b5f4a25b6dd356fcac5f3b
lenovo <lenovo@lenovo-PC>
2018-05-24 23:58:39 +0800

    tianj

---
+++ Test Suites/gaoyou1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>gaoyou1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>dec9c7da-6bbd-4f67-81a2-a3db46b45ac8</testSuiteGuid>
+</TestSuiteEntity>
